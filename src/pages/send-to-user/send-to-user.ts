5438c04ca7f8e95f34f432ecdd98c58fdb68bffe
Daniel Childs <daniel.childs@bjss.com>
2018-03-11 11:34:51 +0000

    Added avatars and select by avatar

--- src/pages/send-to-user/send-to-user.ts
+++ src/pages/send-to-user/send-to-user.ts
@@ -11,6 +11,33 @@ export class SendToUserPage {
   personId: string;
   message: string;
 
+  avatars = [
+    {
+      "id":"8f7df3d1-1fc1-4ce8-a45e-d39c3f759b17",
+      "name":"Ammar",
+      "isActive": false,
+      "image": "assets/imgs/avatars/ammar.jpg"
+    },
+    {
+      "id":"12200e47-bb72-47ab-8057-c5d19d230268",
+      "name":"Callum",
+      "isActive": false,
+      "image": "assets/imgs/avatars/callum.jpg"
+    },
+    {
+      "id":"",
+      "name":"Carl",
+      "isActive": false,
+      "image": "assets/imgs/avatars/carl.jpg"
+    },
+    {
+      "id":"26e04475-3607-47df-a80a-2add7a8f5a5c",
+      "name":"Daniel",
+      "isActive": false,
+      "image": "assets/imgs/avatars/daniel.jpg"
+    },
+  ]; 
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -19,6 +46,19 @@ export class SendToUserPage {
 
   broadcastMessage() {
     this.messageProvider.broadcastToPerson(this.personId, this.message);
+  }
+
+  onSelect(selectedAvatarId: string) {
+    this.avatars.forEach((av) => {
+      if (av.id === selectedAvatarId) {
+        av.isActive = true;
+      }
+      else {
+        av.isActive = false;
+      }
+    });
+
+    this.personId = selectedAvatarId;
   }
 
 }
